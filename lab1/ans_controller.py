439a476c3a381694ee0b53dff0c7a75406e238fc
Daniel Gutter <d.gutter19@gmail.com>
2026-05-14 16:23:57 +0200

    finished implementation; added Test file

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -23,8 +23,9 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet, arp, ipv4, icmp
+from ryu.ofproto import ofproto_v1_3, ether
+from ryu.lib.packet import packet, ethernet, arp, ipv4, in_proto, icmp
+import ipaddress
 
 
 class LearningSwitch(app_manager.RyuApp):
@@ -35,12 +36,19 @@ class LearningSwitch(app_manager.RyuApp):
 
         # Here you can initialize the data structures you want to keep at the controller
         self.forwarding_tables = {}  # Dictionary for each switch
+        self.router_arp_table = {}  # Dictionary for the ARP table on the router
         self.port_to_own_mac = {
             1: "00:00:00:00:01:01",
             2: "00:00:00:00:01:02",
             3: "00:00:00:00:01:03",
         }
         self.port_to_own_ip = {1: "10.0.1.1", 2: "10.0.2.1", 3: "192.168.1.1"}
+        self.routing_table = {
+            ipaddress.IPv4Network("10.0.1.0/24"): 1,
+            ipaddress.IPv4Network("10.0.2.0/24"): 2,
+            ipaddress.IPv4Network("192.168.1.0/24"): 3,
+        }
+        self.pending_packets = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -55,6 +63,12 @@ class LearningSwitch(app_manager.RyuApp):
             parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
         ]
         self.add_flow(datapath, 0, match, actions)
+
+        # Policy flows on router
+        if datapath.id == 3:
+            self.install_icmp_policy_flows(datapath)
+            self.install_tcp_udp_policy_flows(datapath)
+            self.install_gateway_policy_flows(datapath)
 
     # Add a flow entry to the flow-table
     def add_flow(self, datapath, priority, match, actions):
@@ -72,40 +86,129 @@ class LearningSwitch(app_manager.RyuApp):
         datapath = msg.datapath
         inPort = msg.match["in_port"]
 
-        # frame = packet.Packet(msg.data).get_protocol(ethernet.ethernet)
+        frame = packet.Packet(msg.data).get_protocol(ethernet.ethernet)
         arpMessage = packet.Packet(msg.data).get_protocol(arp.arp)
-        # ipPacket = packet.Packet(msg.data).get_protocol(ipv4.ipv4)
-        # icmpMessage = packet.Packet(msg.data).get_protocol(icmp.icmp)
+        ipPacket = packet.Packet(msg.data).get_protocol(ipv4.ipv4)
+        icmpMessage = packet.Packet(msg.data).get_protocol(icmp.icmp)
 
-        if (
-            arpMessage
-            and arpMessage.opcode == 1
-            and arpMessage.dst_ip in self.port_to_own_ip.values()
+        # ARP for gateway MAC
+        if arpMessage and arpMessage.dst_ip in self.port_to_own_ip.values():
+            # save IP/MAC combination
+            self.router_arp_table[arpMessage.src_ip] = arpMessage.src_mac
+
+            # If router gets an ARP request
+            if arpMessage.opcode == 1:
+                arpPacket = self.create_arp_packet(
+                    self.port_to_own_ip[inPort],
+                    arpMessage.src_ip,
+                    self.port_to_own_mac[inPort],
+                    arpMessage.src_mac,
+                    isRequest=False,
+                )
+                actions = [datapath.ofproto_parser.OFPActionOutput(inPort)]
+
+                self.send_packet(
+                    datapath=datapath,
+                    bufferId=datapath.ofproto.OFP_NO_BUFFER,
+                    inPort=datapath.ofproto.OFPP_CONTROLLER,  # packet comes from controller
+                    actionOutputs=actions,
+                    data=arpPacket.data,
+                )
+
+            # If router gets an ARP Reply
+            elif arpMessage.opcode == 2:
+                # send pending packets if any exist
+                if arpMessage.src_ip in self.pending_packets:
+                    packets = self.pending_packets.pop(arpMessage.src_ip)
+                    outPort = packets[0][1]
+
+                    # same flow for rewriting packets for that dstMAC
+                    actions = self.install_router_flow(
+                        datapath,
+                        self.port_to_own_mac[outPort],
+                        arpMessage.src_mac,
+                        arpMessage.src_ip,
+                        outPort,
+                    )
+
+                    for msg, _ in packets:
+                        self.send_packet(
+                            datapath=datapath,
+                            bufferId=datapath.ofproto.OFP_NO_BUFFER,
+                            inPort=datapath.ofproto.OFPP_CONTROLLER,  # packet comes from controller
+                            actionOutputs=actions,
+                            data=msg.data,
+                        )
+
+        # Ping Reply for gateway
+        elif (
+            ipPacket
+            and frame
+            and ipPacket.dst in self.port_to_own_ip.values()
+            and icmpMessage
+            and icmpMessage.type == icmp.ICMP_ECHO_REQUEST
         ):
-            pkt = packet.Packet()
-            ethHeader = ethernet.ethernet(
-                dst=arpMessage.src_mac,
-                src=self.port_to_own_mac[inPort],
-                ethertype=0x0806,  # ARP CODE
+            icmpPacket = self.create_icmp_packet(
+                self.port_to_own_mac[inPort],
+                frame.src,
+                self.port_to_own_ip[inPort],
+                ipPacket.src,
+                icmpMessage.data,
+                isRequest=False,
             )
-            arpHeader = arp.arp(
-                dst_ip=arpMessage.src_ip,
-                src_ip=self.port_to_own_ip[inPort],
-                dst_mac=arpMessage.src_mac,
-                src_mac=self.port_to_own_mac[inPort],
-                opcode=2,
-            )
-            pkt.add_protocol(ethHeader)
-            pkt.add_protocol(arpHeader)
-            pkt.serialize()
             actions = [datapath.ofproto_parser.OFPActionOutput(inPort)]
-            self.sendPacket(
+
+            self.send_packet(
                 datapath=datapath,
                 bufferId=datapath.ofproto.OFP_NO_BUFFER,
-                inPort=datapath.ofproto.OFPP_CONTROLLER,  # no port, because packet comes from controller
+                inPort=datapath.ofproto.OFPP_CONTROLLER,  # packet comes from controller
                 actionOutputs=actions,
-                data=pkt.data,
+                data=icmpPacket.data,
             )
+
+        # Routing IP Packets to correct Port
+        elif ipPacket:
+            outPort = self.find_longest_matching_prefix(ipPacket.dst)
+
+            if ipPacket.dst not in self.router_arp_table:
+                # save packet for later
+                if ipPacket.dst not in self.pending_packets:
+                    self.pending_packets[ipPacket.dst] = []
+                self.pending_packets[ipPacket.dst].append((msg, outPort))
+
+                # ARP for dst IP
+                arpPacket = self.create_arp_packet(
+                    self.port_to_own_ip[outPort],
+                    ipPacket.dst,
+                    self.port_to_own_mac[outPort],
+                    "ff:ff:ff:ff:ff:ff",
+                    isRequest=True,
+                )
+                actions = [datapath.ofproto_parser.OFPActionOutput(outPort)]
+
+                self.send_packet(
+                    datapath=datapath,
+                    bufferId=datapath.ofproto.OFP_NO_BUFFER,
+                    inPort=datapath.ofproto.OFPP_CONTROLLER,  # packet comes from controller
+                    actionOutputs=actions,
+                    data=arpPacket.data,
+                )
+            else:
+                # if the dstMAC is already in the table, we can rewrite the frame directly
+                actions = self.install_router_flow(
+                    datapath,
+                    self.port_to_own_mac[outPort],
+                    self.router_arp_table[ipPacket.dst],
+                    ipPacket.dst,
+                    outPort,
+                )
+                self.send_packet(
+                    datapath=datapath,
+                    bufferId=datapath.ofproto.OFP_NO_BUFFER,
+                    inPort=datapath.ofproto.OFPP_CONTROLLER,  # packet comes from controller
+                    actionOutputs=actions,
+                    data=msg.data,
+                )
 
     def perform_switch_logic(self, msg):
         datapath = msg.datapath
@@ -121,7 +224,7 @@ class LearningSwitch(app_manager.RyuApp):
 
         self.forwarding_tables[switchId][sourceMAC] = inPort
 
-        # just set the port
+        # If destMAC already known, create flow and forward
         if destMAC in self.forwarding_tables[switchId]:
             actions = [
                 datapath.ofproto_parser.OFPActionOutput(
@@ -130,18 +233,157 @@ class LearningSwitch(app_manager.RyuApp):
             ]
             match = datapath.ofproto_parser.OFPMatch(eth_dst=destMAC, in_port=inPort)
             self.add_flow(datapath, 1, match, actions)
+        # If not, flood
         else:
             actions = [
                 datapath.ofproto_parser.OFPActionOutput(datapath.ofproto.OFPP_FLOOD)
             ]
-        self.sendPacket(
+        self.send_packet(
             datapath, datapath.ofproto.OFP_NO_BUFFER, inPort, actions, msg.data
         )
 
-    def sendPacket(self, datapath, bufferId, inPort, actionOutputs, data):
+    def install_router_flow(self, datapath, srcMAC, dstMAC, dstIP, outPort):
+        actions = [
+            datapath.ofproto_parser.OFPActionSetField(eth_src=srcMAC),
+            datapath.ofproto_parser.OFPActionSetField(eth_dst=dstMAC),
+            datapath.ofproto_parser.OFPActionOutput(outPort),
+        ]
+
+        # since we know the dstMAC, we can create a flow for rewriting the packtes
+        match = datapath.ofproto_parser.OFPMatch(
+            eth_type=ether.ETH_TYPE_IP, ipv4_dst=dstIP
+        )
+        self.add_flow(datapath, 10, match, actions)
+
+        return actions
+
+    def install_icmp_policy_flows(self, datapath):
+        ext_net = next(net for net, port in self.routing_table.items() if port == 3)
+        int_nets = [net for net, port in self.routing_table.items() if port in (1, 2)]
+
+        for int_net in int_nets:
+            # external to internal
+            match = datapath.ofproto_parser.OFPMatch(
+                eth_type=ether.ETH_TYPE_IP,
+                ip_proto=in_proto.IPPROTO_ICMP,
+                icmpv4_type=icmp.ICMP_ECHO_REQUEST,
+                # apply subnet mask by converting to tuple
+                ipv4_src=(str(ext_net.network_address), str(ext_net.netmask)),
+                ipv4_dst=(str(int_net.network_address), str(int_net.netmask)),
+            )
+            self.add_flow(datapath, 100, match, [])
+
+            # internal to external
+            match = datapath.ofproto_parser.OFPMatch(
+                eth_type=ether.ETH_TYPE_IP,
+                ip_proto=in_proto.IPPROTO_ICMP,
+                icmpv4_type=icmp.ICMP_ECHO_REQUEST,
+                ipv4_src=(str(int_net.network_address), str(int_net.netmask)),
+                ipv4_dst=(str(ext_net.network_address), str(ext_net.netmask)),
+            )
+            self.add_flow(datapath, 100, match, [])
+
+    def install_tcp_udp_policy_flows(self, datapath):
+        ext_net = next(net for net, port in self.routing_table.items() if port == 3)
+        ser_net = next(net for net, port in self.routing_table.items() if port == 2)
+
+        for proto in (in_proto.IPPROTO_TCP, in_proto.IPPROTO_UDP):
+            # external to server
+            match = datapath.ofproto_parser.OFPMatch(
+                eth_type=ether.ETH_TYPE_IP,
+                ip_proto=proto,
+                # apply subnet mask by converting to tuple
+                ipv4_src=(str(ext_net.network_address), str(ext_net.netmask)),
+                ipv4_dst=(str(ser_net.network_address), str(ser_net.netmask)),
+            )
+            self.add_flow(datapath, 100, match, [])
+
+            # server to external
+            match = datapath.ofproto_parser.OFPMatch(
+                eth_type=ether.ETH_TYPE_IP,
+                ip_proto=proto,
+                ipv4_src=(str(ser_net.network_address), str(ser_net.netmask)),
+                ipv4_dst=(str(ext_net.network_address), str(ext_net.netmask)),
+            )
+            self.add_flow(datapath, 100, match, [])
+
+    def install_gateway_policy_flows(self, datapath):
+        for in_port in self.port_to_own_ip:
+            for out_port, gateway_ip in self.port_to_own_ip.items():
+                # install flow rule when ping doesnt go the same port/gateway where its coming from
+                if in_port != out_port:
+                    match = datapath.ofproto_parser.OFPMatch(
+                        in_port=in_port,
+                        eth_type=ether.ETH_TYPE_IP,
+                        ip_proto=in_proto.IPPROTO_ICMP,
+                        icmpv4_type=icmp.ICMP_ECHO_REQUEST,
+                        ipv4_dst=gateway_ip,
+                    )
+                    self.add_flow(datapath, 100, match, [])
+
+    def send_packet(self, datapath, bufferId, inPort, actionOutputs, data):
         ofp_parser = datapath.ofproto_parser
         out = ofp_parser.OFPPacketOut(datapath, bufferId, inPort, actionOutputs, data)
         datapath.send_msg(out)
+
+    def create_arp_packet(self, srcIP, dstIP, srcMAC, dstMAC, isRequest: bool):
+        pkt = packet.Packet()
+        ethHeader = ethernet.ethernet(
+            dst=dstMAC, src=srcMAC, ethertype=ether.ETH_TYPE_ARP  # ARP Code
+        )
+        arpHeader = arp.arp(
+            dst_ip=dstIP,
+            src_ip=srcIP,
+            dst_mac="00:00:00:00:00:00" if isRequest else dstMAC,
+            src_mac=srcMAC,
+            opcode=1 if isRequest else 2,  # 1 = request; 2 = response
+        )
+        pkt.add_protocol(ethHeader)
+        pkt.add_protocol(arpHeader)
+        pkt.serialize()
+        return pkt
+
+    def create_icmp_packet(
+        self, srcMAC, dstMAC, srcIP, dstIP, icmpData, isRequest: bool
+    ):
+        pkt = packet.Packet()
+        ethHeader = ethernet.ethernet(
+            dst=dstMAC,
+            src=srcMAC,
+            ethertype=ether.ETH_TYPE_IP,
+        )
+        ipHeader = ipv4.ipv4(
+            dst=dstIP,
+            src=srcIP,
+            proto=in_proto.IPPROTO_ICMP,
+        )
+        # set Type to Ping request/reply
+        icmpHeader = icmp.icmp(
+            type_=icmp.ICMP_ECHO_REQUEST if isRequest else icmp.ICMP_ECHO_REPLY,
+            code=(
+                icmp.ICMP_ECHO_REQUEST_CODE if isRequest else icmp.ICMP_ECHO_REPLY_CODE
+            ),
+            csum=0,
+            data=icmpData,
+        )
+
+        pkt.add_protocol(ethHeader)
+        pkt.add_protocol(ipHeader)
+        pkt.add_protocol(icmpHeader)
+        pkt.serialize()
+        return pkt
+
+    def find_longest_matching_prefix(self, ip):
+        ip_address = ipaddress.IPv4Address(ip)
+        current_max_length = 0
+        port = None
+
+        for network, outPort in self.routing_table.items():
+            if ip_address in network and network.prefixlen > current_max_length:
+                port = outPort
+                current_max_length = network.prefixlen
+
+        return port
 
     # Handle the packet_in event
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -157,9 +399,3 @@ class LearningSwitch(app_manager.RyuApp):
         # Switch logic
         else:
             self.perform_switch_logic(msg)
-
-        for switch, table in self.forwarding_tables.items():
-            print(f"Switch {switch}:")
-            for mac, port in table.items():
-                print(f"  {mac} -> port {port}")
-        print("-----------------------------------")
--- lab1/run_network.py
+++ lab1/run_network.py
@@ -35,23 +35,11 @@ class NetworkTopo(Topo):
 
         Topo.__init__(self)
 
-        self.h1 = self.addHost(
-            "h1", ip="10.0.1.2/24", defaultRoute="via 10.0.1.1", mac="00:00:00:00:00:01"
-        )
-        self.h2 = self.addHost(
-            "h2", ip="10.0.1.3/24", defaultRoute="via 10.0.1.1", mac="00:00:00:00:00:02"
-        )
-        self.ser = self.addHost(
-            "ser",
-            ip="10.0.2.2/24",
-            defaultRoute="via 10.0.2.1",
-            mac="00:00:00:00:00:10",
-        )
+        self.h1 = self.addHost("h1", ip="10.0.1.2/24", defaultRoute="via 10.0.1.1")
+        self.h2 = self.addHost("h2", ip="10.0.1.3/24", defaultRoute="via 10.0.1.1")
+        self.ser = self.addHost("ser", ip="10.0.2.2/24", defaultRoute="via 10.0.2.1")
         self.ext = self.addHost(
-            "ext",
-            ip="192.168.1.123/24",
-            defaultRoute="via 192.168.1.1",
-            mac="00:00:00:00:00:20",
+            "ext", ip="192.168.1.123/24", defaultRoute="via 192.168.1.1"
         )
 
         self.s1 = self.addSwitch("s1", cls=OVSKernelSwitch)
